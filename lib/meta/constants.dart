cf756d919cc0369f17b154ebe9affeffa6ad8b72
hemangdtu <hemang.dtu@gmail.com>
2021-04-09 18:27:53 +0530

    Initial Commit

---
+++ lib/meta/constants.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+var appBarTitleText = {
+  0: "Tools",
+  1: "Disease Prediction",
+  2: "Yield Prediction",
+  3: "News",
+  4: "Marketplace",
+};
+
+var appBarBackgroundColor = {
+  0: Colors.green,
+  1: Colors.red,
+  2: Colors.purple,
+  3: Colors.black,
+  4: Colors.deepPurple,
+};
+
+var navBarIcons = {
+  0: Icons.build,
+  1: Icons.coronavirus_outlined,
+  2: Icons.spa,
+  3: Icons.book,
+  4: Icons.store,
+};
+var navBarTitle = {
+  0: "Tools",
+  1: "Disease",
+  2: "Yield",
+  3: "News",
+  4: "Marketplace",
+};
---
+++ lib/widgets/templates.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Widget NavBarText(String titleText) {
+  return Text(
+    titleText,
+    style: TextStyle(color: Colors.white),
+  );
+}
